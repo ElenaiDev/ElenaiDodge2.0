56b42542c745c73f0378ae0428109ff85c5e257c
Elenai <60099052+ElenaiDev@users.noreply.github.com>
2021-02-28 10:52:41 +0000

    Updated Version

--- src/main/java/com/elenai/elenaidodge2/ElenaiDodge2.java
+++ src/main/java/com/elenai/elenaidodge2/ElenaiDodge2.java
@@ -18,7 +18,7 @@ public class ElenaiDodge2 {
 	public static final Logger LOGGER = LogManager.getLogger();
 	public static final String NAME = "Elenai Dodge";
 	public static final String MODID = "elenaidodge2";
-    public static final String VERSION = "1.0.8";
+    public static final String VERSION = "1.0.8b";
 	
 	public static final Logger LOG = LogManager.getLogger("ElenaiDodge");
 	
